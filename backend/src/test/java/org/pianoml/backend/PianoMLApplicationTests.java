db01d20a8878f4e23bd995e3144da21a79dc91b4
Emmanuel Florent <emmanuel.florent@gmail.com>
2025-08-06 15:05:49 +0200

    initialize backend

---
+++ backend/src/test/java/org/pianoml/backend/PianoMLApplicationTests.java
@@ -0,0 +1,13 @@
+package org.pianoml.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PianoMLApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
