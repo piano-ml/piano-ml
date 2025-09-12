391d85d081581e3da4b406d945bd92f0be9174d7
Emmanuel Florent <emmanuel.florent@gmail.com>
2025-09-12 16:32:26 +0200

    improved pdf packing

--- backend/scripts/pdf2pack.sh
+++ backend/scripts/pdf2pack.sh
@@ -26,7 +26,7 @@ FILES=$(ls -p "$FROOT"*.png)
 XMLFILES=""
 for FILE in $FILES; do
   echo "starting homr $FILE"
- poetry -v run homr "$FILE" || exit 1
+  cd /homr && poetry -v run homr "$FILE" || exit 1
   XML_FILE="${FILE%.png}.musicxml"
   if [ -z "$XMLFILES" ]; then
     XMLFILES="$XML_FILE"
