2af4da2e9469ff68c6bd786ee22f4ee17fa80215
Emmanuel Florent <emmanuel.florent@gmail.com>
2025-12-03 11:45:04 +0100

    fix public/copyright encoding

--- backend/src/main/java/org/pianoml/backend/service/ScoreService.java
+++ backend/src/main/java/org/pianoml/backend/service/ScoreService.java
@@ -111,7 +111,9 @@ public class ScoreService {
       // set EU public domain status if possible
       score.setPublicDomain(score.getAuthor().getLifeSpanEnd().isBefore (LocalDate.now().minusYears(70)));
     } else {
-      score.setPublicDomain(false);
+      if (score.getAuthor().getLifeSpanBegin()!=null) {
+        score.setPublicDomain(false);
+      }
     }
     score.setPlayCount(0L);
 
