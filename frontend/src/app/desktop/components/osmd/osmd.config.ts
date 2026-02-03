8e3d3cb0b4c70f437376260e791d761e2d29ad39
Emmanuel Florent <emmanuel.florent@gmail.com>
2026-02-03 21:58:11 +0100

    fallback imprecise cursor

--- frontend/src/app/desktop/components/osmd/osmd.config.ts
+++ frontend/src/app/desktop/components/osmd/osmd.config.ts
@@ -18,7 +18,7 @@ export const DEFAULT_OSMD_OPTIONS: IOSMDOptions = {
             follow: true, 
             color: "#B0F2B4",
             alpha: 1,
-            type: 4
+            type: 3
         },
     ] as CursorOptions[],
     drawTitle: false,
