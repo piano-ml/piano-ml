99717cdce24242559be11e155170c0f2540e25be
Emmanuel Florent <emmanuel.florent@gmail.com>
2026-02-07 18:52:14 +0100

    fixprerender

--- frontend/src/type.d.ts
+++ frontend/src/type.d.ts
@@ -2,3 +2,8 @@ declare module "*.glsl" {
     const content: string;
     export default content;
   }
+
+declare module './angular-app-engine-manifest.mjs' {
+    const manifest: any;
+    export default manifest;
+}
