62cc59a4ec17c47437132920e83b30911078b3e7
Florent <emmanuel.florent@gmail.com>
2026-05-22 18:43:16 +0200

    better midi import (#296)
    * better midi import
    
    * better midi import

--- backend/scripts/midi2pack.sh
+++ backend/scripts/midi2pack.sh
@@ -67,6 +67,10 @@ if [ -n "$TRACK_RIGHT" ]; then
   else
     printf "Extracting track %s for %s.midi (TRACK_LEFT omitted)\n" "$TRACK_RIGHT" "$FROOT"
     python ./scripts/extract_midi_tracks.py "${FROOT}.musicxml" "$TRACK_RIGHT" > /dev/null 2>&1
+    sleep 1
+    musescore3 -o "${FROOT}.mid" "${FROOT}.musicxml" > /dev/null 2>&1
+    musescore3 -M midioperations.xml -o "${FROOT}.musicxml" "${FROOT}.mid"
+    #mv -- "${FROOT}.mid" "${FROOT}.midi"
   fi
 else
   printf "No track specified in original midi file\n" >&2
